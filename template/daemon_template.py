cd7312116d343a4c0d9a8ceed2f5f1505e329625
Jason <jason.jia.chen@gmail.com>
2023-12-28 16:24:05 -0800

    Update README and template

--- template/daemon_template.py
+++ template/daemon_template.py
@@ -1,14 +1,13 @@
-# custom_daemon.py
 from daemon_creator.daemon import Daemon
 
-def my_custom_task():
+
+def user_task():
     # User's specific task logic
-    # For example: perform some computation, check a condition, etc.
-    # Let's assume the task creates a message string
     message = "Performing my task..."
     my_daemon.log_task_output(message)
 
+
 if __name__ == "__main__":
     daemon_name = "daemon_template"
-    my_daemon = Daemon(name=daemon_name, task_function=my_custom_task)
+    my_daemon = Daemon(name=daemon_name, task_function=user_task)
     my_daemon.start()
